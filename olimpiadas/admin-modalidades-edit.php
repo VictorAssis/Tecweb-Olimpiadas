7151ecfa32f38fc94a7197dd773522a2affd46fc
Victor Oliveira <Victor Oliveira>
2016-03-27 13:43:20 -0300

    Edição de modalidade implementada

--- olimpiadas/admin-modalidades-edit.php
+++ olimpiadas/admin-modalidades-edit.php
@@ -1,37 +1,81 @@
 <?php
     include("init.php");
+
+    $modalidade = new Modalidade();
+    $upload = new Upload();
+
+    if (isset($_POST['btnSalvar'])) {        
+        if ($_FILES['foto_destaque']['tmp_name'] != '')
+            $foto_destaque = $upload->uploadFile($_FILES['foto_destaque']);
+        else
+            $foto_destaque = $_POST['foto_destaque'];
+        if ($_FILES['foto_lista']['tmp_name'] != '')
+            $foto_lista = $upload->uploadFile($_FILES['foto_lista']);
+        else
+            $foto_lista = $_POST['foto_lista'];
+
+        $modalidade->id = $_POST['id'];
+        $modalidade->nome = $_POST['nome'];
+        $modalidade->foto_destaque = $foto_destaque;
+        $modalidade->foto_lista = $foto_lista;
+        $modalidade->finalidade = $_POST['finalidade'];
+        $modalidade->provas = $_POST['provas'];
+        $modalidade->estreia = $_POST['estreia'];
+        $modalidade->regras = $_POST['regras'];
+
+        if ($modalidade->update()) {
+            mensagemSucesso("Modalidade alterada com sucesso.");
+            header("Location: admin-modalidades-listar.php");
+            die();
+        } else 
+            mensagemErro("Falha ao alterar modalidade, tente novamente.");
+    }
+
+    $item = $modalidade->findById($_GET['id']);
+    $item = $item[0];
     include("header-admin.php");
 ?>
 <section id="content" class="container">
 	<h1 class="titulo-pagina">Editar Modalidade <a class="link-voltar" href="javascript: history.go(-1);">< Voltar</a></h1>
-	<form>
+	<form enctype="multipart/form-data" action="" method="post">
+        <input type="hidden" name="id" value="<?php echo $item['id']; ?>" />
         <p class="form-group">
             <label for="nome">Nome</label>
-            <input type="text" id="nome" class="campo" placeholder="Futebol" required="required" />
+            <input type="text" name="nome" id="nome" value="<?php echo $item['nome']; ?>" class="campo" placeholder="Futebol, Volei, etc..." required="required" maxlength="100"/>
         </p>
-        <p class="form-group">
-            <label for="foto">Foto destaque</label>
-            <input type="file" id="foto" class="campo" required="required" />
+        <p class="form-group uploadEdit">
+            <label for="foto_destaque">Foto destaque</label>
+            <img class="preview-foto" src="uploads/<?php echo $item['foto_destaque']; ?>" />
+            <a class="alterar-foto">Alterar foto</a>
+            <input type="file" name="foto_destaque" id="foto_destaque" class="campo"/>
+            <input type="hidden" name="foto_destaque" value="<?php echo $item['foto_destaque']; ?>"/>
+        </p>
+        <p class="form-group uploadEdit">
+            <label for="foto_lista">Foto lista</label>
+            <img class="preview-foto" src="uploads/<?php echo $item['foto_lista']; ?>" />
+            <a class="alterar-foto">Alterar foto</a>
+            <input type="file" name="foto_lista" id="foto_lista" class="campo"/>
+            <input type="hidden" name="foto_lista" value="<?php echo $item['foto_lista']; ?>"/>
         </p>
         <p class="form-group">
             <label for="finalidade">Finalidade</label>
-            <textarea id="finalidade" class="campo" required="required"></textarea>
+            <textarea name="finalidade" id="finalidade" class="campo" required="required"><?php echo $item['finalidade']; ?></textarea>
         </p>
         <p class="form-group">
-            <label for="Provas">Provas</label>
-            <textarea id="Provas" class="campo tinyEditor" required="required"></textarea>
+            <label for="provas">Provas</label>
+            <textarea name="provas" id="provas" class="campo tinyEditor"><?php echo $item['provas']; ?></textarea>
         </p>
         <p class="form-group">
             <label for="estreia">Estréia nos Jogos Olímpicos</label>
-            <input type="text" id="estreia" class="campo" placeholder="Atenas 1986" required="required" />
+            <input type="text" name="estreia" id="estreia" value="<?php echo $item['estreia']; ?>" class="campo" placeholder="Atenas 1986" required="required" maxlength="255" />
         </p>
         <p class="form-group">
             <label for="regras">Regras do Jogo</label>
-            <textarea id="regras" class="campo tinyEditor" required="required"></textarea>
-
-        <p class="form-group">
-            <input type="submit" class="botao" value="Cadastrar" />
+            <textarea name="regras" id="regras" class="campo tinyEditor"><?php echo $item['regras']; ?></textarea>
         </p>
-	</form>
+        <p class="form-group">
+            <input type="submit" name="btnSalvar" class="botao" value="Salvar" />
+        </p>
+    </form>
 </section>
 <?php include("footer-admin.php"); ?>
--- olimpiadas/footer-admin.php
+++ olimpiadas/footer-admin.php
@@ -12,67 +12,77 @@
 		$(document).ready(function(){
 			// Máscaras
 			$(".campodata").mask("99/99/9999");
-       		$(".campocpf").mask("999.999.99-99");
-       		$(".campofone").mask("(99)9999-9999?9");
-       		$(".campopreco").mask("9,99?9",{placeholder:" "}).keyup(function(){
-       			valorAtual = $(this).val();
-       			//console.log(valorAtual.substring(valorAtual.length - 1));
-       			if (valorAtual.substring(valorAtual.length - 1) != " ") {
-       				$(this).unmask();
-       				mascara = "";
-       				for(var i = 0;i < valorAtual.length - 1;i++) {
-       					if (i == valorAtual.length - 3)
-       						mascara += ",";
-       					mascara += "9";
-       				}
-       				mascara += "?9";
-       				$(this).val(valorAtual.replace(",","")).mask(mascara,{placeholder:" "});
-       			}
-       		});
+   		$(".campocpf").mask("999.999.99-99");
+   		$(".campofone").mask("(99)9999-9999?9");
+   		$(".campopreco").mask("9,99?9",{placeholder:" "}).keyup(function(){
+   			valorAtual = $(this).val();
+   			//console.log(valorAtual.substring(valorAtual.length - 1));
+   			if (valorAtual.substring(valorAtual.length - 1) != " ") {
+   				$(this).unmask();
+   				mascara = "";
+   				for(var i = 0;i < valorAtual.length - 1;i++) {
+   					if (i == valorAtual.length - 3)
+   						mascara += ",";
+   					mascara += "9";
+   				}
+   				mascara += "?9";
+   				$(this).val(valorAtual.replace(",","")).mask(mascara,{placeholder:" "});
+   			}
+   		});
 
-       		//Multiplo select
-       		$(".selectMultiple").multipleSelect({
-       			selectAllText: "Selecionar todos",
-       			selectAllDelimiter: ['(',')'],
-       			allSelected: "Todos selecionados",
-       			width: "100%"
-       		});
+   		//Multiplo select
+   		$(".selectMultiple").multipleSelect({
+   			selectAllText: "Selecionar todos",
+   			selectAllDelimiter: ['(',')'],
+   			allSelected: "Todos selecionados",
+   			width: "100%"
+   		});
 
-       		//Editor de texto completo
-       		tinymce.init({ selector:'.tinyEditor' });
+   		//Editor de texto completo
+   		tinymce.init({ selector:'.tinyEditor' });
 
-                     //Gráficos Dashboard
-                     var data = {
-                         labels: ["Vôlei de praia", "Voleibol", "Ginástica artística", "Futebol"],
-                         datasets: [
-                             {
-                                 label: "My First dataset",
-                                 fillColor: "rgba(220,220,220,0.5)",
-                                 strokeColor: "rgba(220,220,220,0.8)",
-                                 highlightFill: "rgba(220,220,220,0.75)",
-                                 highlightStroke: "rgba(220,220,220,1)",
-                                 data: [65, 59, 80, 81, 56, 55, 40]
-                             }
-                         ]
-                     };
-                     var ctx = $("#grafico1").get(0).getContext("2d");
-                     var myNewChart = new Chart(ctx).Bar(data);
+      //Edição de foto
+      $(".uploadEdit").each(function(){
+        var container = $(this);
+        container.find("input").hide();
+        container.find(".alterar-foto").on('click',function(){
+          container.find("input").show().attr("required","required");
+          container.find(".alterar-foto,.preview-foto").hide();
+        });
+      });
 
-                     var data2 = {
-                         labels: ["Coapacabana", "Barra da Tijuca", "Belo Horizonte", "São Paulo"],
-                         datasets: [
-                             {
-                                 label: "My First dataset",
-                                 fillColor: "rgba(220,220,220,0.5)",
-                                 strokeColor: "rgba(220,220,220,0.8)",
-                                 highlightFill: "rgba(220,220,220,0.75)",
-                                 highlightStroke: "rgba(220,220,220,1)",
-                                 data: [65, 59, 80, 81, 56, 55, 40]
-                             }
-                         ]
-                     };
-                     var ctx2 = $("#grafico2").get(0).getContext("2d");
-                     var myNewChart2 = new Chart(ctx2).Bar(data2);
+      //Gráficos Dashboard
+      var data = {
+         labels: ["Vôlei de praia", "Voleibol", "Ginástica artística", "Futebol"],
+         datasets: [
+             {
+                 label: "My First dataset",
+                 fillColor: "rgba(220,220,220,0.5)",
+                 strokeColor: "rgba(220,220,220,0.8)",
+                 highlightFill: "rgba(220,220,220,0.75)",
+                 highlightStroke: "rgba(220,220,220,1)",
+                 data: [65, 59, 80, 81, 56, 55, 40]
+             }
+         ]
+      };
+      var ctx = $("#grafico1").get(0).getContext("2d");
+      var myNewChart = new Chart(ctx).Bar(data);
+
+      var data2 = {
+         labels: ["Coapacabana", "Barra da Tijuca", "Belo Horizonte", "São Paulo"],
+         datasets: [
+             {
+                 label: "My First dataset",
+                 fillColor: "rgba(220,220,220,0.5)",
+                 strokeColor: "rgba(220,220,220,0.8)",
+                 highlightFill: "rgba(220,220,220,0.75)",
+                 highlightStroke: "rgba(220,220,220,1)",
+                 data: [65, 59, 80, 81, 56, 55, 40]
+             }
+         ]
+      };
+      var ctx2 = $("#grafico2").get(0).getContext("2d");
+      var myNewChart2 = new Chart(ctx2).Bar(data2);
 		});
 	</script>
 </body>
